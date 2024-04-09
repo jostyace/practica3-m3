04143a031d0d9cc75654e2ddbc2b7c86cdc1934a
jostyace <jostyace@gmail.com>
2024-04-09 14:29:21 -0500

    Brach created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './index.css'
+import { MainScreen } from './Components/mainScreen'
 function App() {
 
 
   return (
     <>
-
+      <MainScreen />
+      <h1>Hola</h1>
     </>
   )
 }
